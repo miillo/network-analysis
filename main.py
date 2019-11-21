9ecf577bd141df45c280271fad0c112bc39e6c66
Miłosz Pluta <milosz.pluta2@gmail.com>
2019-11-21 21:43:47 +0100

    Refactor v1

--- main.py
+++ main.py
@@ -9,151 +9,150 @@ from sklearn.linear_model import LinearRegression
 import powerlaw
 
 
-def print_k_cores(graph):
+# Wypisuje posortowane k-rdzenie oraz liczbę wierzchołków do nich należącą
+def no_of_nodes_for_k_cores(graph):
+    # liczba rdzeni dla każdego wierzchołka
     core_numbers = nx.core_number(graph)
 
-    # count number of nodes for each k-core
+    # zliczenie liczby wierzchołków dla każdego rdzenia
     inv_map = {}
     for k, v in core_numbers.items():
         inv_map[v] = inv_map.get(v, [])
         inv_map[v].append(k)
 
-    # aggregate nodes to list length
     mapped = {k: len(v) for k, v in inv_map.items()}
-    # sort by k and print
     sorted_core_numbers = collections.OrderedDict(sorted(mapped.items(), key=lambda kv: kv[0]))
-    print(sorted_core_numbers)
+    return sorted_core_numbers
 
 
-# read graph
+# wczytanie grafu
 G = nx.read_adjlist('C:\\Users\\Milosz\\Desktop\\TASS_Project_I\\Ex2_data\\3.txt')
 
-# print('Original graph parameters')
-# print('No. of nodes: ' + str(G.number_of_nodes()))
-# print('No. of edges: ' + str(G.number_of_edges()))
+# # # # # zbadaj jaki jest rząd i rozmiar całej sieci: pierwotnej oraz po usunięciu pętli i duplikatów krawędzi # # # #
 
+# print('Rząd i rozmiar sieci pierwotnej')
+# print('Liczba wierzchołków(rząd): ' + str(G.number_of_nodes()))
+# print('Liczba krawędzi(rozmiar): ' + str(G.number_of_edges()))
+#
 # print()
 
-# remove self-loops
+# usunięcie pętli
 G.remove_edges_from(nx.selfloop_edges(G))
-# Graph is loaded as 'networkx.classes.graph.Graph' class instance - it means it can't have duplicated edges
+# graf jest wczytany jako obiekt typu: 'networkx.classes.graph.Graph' - tzn. nie ma zduplikowanych krawędzi
 
-# print('Graph without loops / double edges parameters')
-# print('No. of nodes: ' + str(G.number_of_nodes()))
-# print('No. of edges: ' + str(G.number_of_edges()))
-
+# print('Rząd i rozmiar sieci po usunięciu pętli / duplikató krawędzi')
+# print('Liczba wierzchołków(rząd): ' + str(G.number_of_nodes()))
+# print('Liczba krawędzi(rozmiar): ' + str(G.number_of_edges()))
+#
 # print()
 
-# test number of components -> nx.number_connected_components(G)
+# # # # # wyodrębnij największą składową spójną, zbadaj jej rząd i rozmiar # # # #
+
+# sprawdzenie liczby komponentów
+# print('Liczba komponentów: ' + str(nx.number_connected_components(G)))
 
 # generate largest connected component
 largest_cc = max(nx.connected_components(G), key=len)
 largest_cc_graph = G.subgraph(largest_cc)
 
-# print('Largest connected component parameters')
-# print('No. of nodes: ' + str(largest_cc_graph.number_of_nodes()))
-# print('No. of edges: ' + str(largest_cc_graph.number_of_edges()))
+# print('Rząd i rozmiar największej składowej spójnej')
+# print('Liczba wierzchołków(rząd): ' + str(largest_cc_graph.number_of_nodes()))
+# print('Liczba krawędzi(rozmiar): ' + str(largest_cc_graph.number_of_edges()))
 
-# approximation of average path length with three random samples: 100 / 1k / 10k
-# approxPathLength = 0
-# noOfNodes = largest_cc_graph.number_of_nodes()
-# nodes = largest_cc_graph.nodes()
+# # # # # wyznacz aproksymacje średniej długości ścieżki,
+# # # # # operując na próbie losowej 100, 1000 i 10 tys. par wierzchołków
 
-# print('@@@@ ' + str(noOfNodes))
+# TODO dla trzech prob
 
-# for i in range(0, 10000):
-#    print('# ' + str(i))
-#    node1 = choice(list(nodes))
-#    node2 = choice(list(nodes))
-#    shortest_path_length = nx.dijkstra_path_length(largest_cc_graph, node1, node2)
-#    approxPathLength += shortest_path_length
+sumOfShortestPaths = 0
+noOfNodes = largest_cc_graph.number_of_nodes()
+nodes = largest_cc_graph.nodes()
 
-# print(approxPathLength / (noOfNodes * (noOfNodes - 1)))
+for i in range(0, 10):
+    node1 = choice(list(nodes))
+    node2 = choice(list(nodes))
+    shortest_path_length = nx.dijkstra_path_length(largest_cc_graph, node1, node2)
+    sumOfShortestPaths += shortest_path_length
+    print('Iteration: ' + str(i) + ' | Current sum: ' + str(sumOfShortestPaths))
 
-print()
+print(sumOfShortestPaths / (noOfNodes * (noOfNodes - 1)))
 
-# #######################T O D O################################
-# find no. of k-cores with highest possible k
-# print('First shot')
-# print_k_cores(largest_cc_graph)
+# print()
+
+# # # # # wyznacz liczbę rdzeni o największym możliwym rzędzie, o drugim możliwie największym rzędzie
+# # # # # o trzecim możliwie największym rzędzie; jakie to są rzędy? # # # #
+
+# 'odmrożenie' grafu w celu umożliwienia wykonania na nim operacji
+# unfrezeed = nx.Graph(largest_cc_graph)
 #
-# # unfreeze graph
-# newOne = nx.Graph(largest_cc_graph)
+# print('Pierwsze sprawdzenie liczby rdzeni: ' + str(no_of_nodes_for_k_cores(unfrezeed)))  # max 155
 #
-# print('\nSecond shot')
-# to_del1 = [node for node, degree in dict(newOne.degree()).items() if degree > 155]
-# newOne.remove_nodes_from(to_del1)
-# print_k_cores(newOne)
+# # wyznaczenie wierzchołków do usunięcia na podstawie ostatniego odczytu
+# to_del1 = [node for node, degree in dict(unfrezeed.degree()).items() if degree >= 155]
+# unfrezeed.remove_nodes_from(to_del1)
+#
+# print('Drugie sprawdzenie liczby rdzenii: ' + str(no_of_nodes_for_k_cores(unfrezeed))) # max 67
+#
+# # wyznaczenie wierzchołków do usunięcia na podstawie ostatniego odczytu
+# to_del2 = [node for node, degree in dict(unfrezeed.degree()).items() if degree >= 67]
+# unfrezeed.remove_nodes_from(to_del2)
+#
+# print('Trzecie sprawdzenie liczby rdzenii: ' + str(no_of_nodes_for_k_cores(unfrezeed))) # max 53
 
-# print('\nThird shot')
-# to_del2 = [node for node, degree in dict(largest_cc_graph.degree()).items() if degree > 155]
-# ts = ss.remove_nodes_from(to_del2)
-# print_k_cores(ts)
+# # # # wykreśl rozkład stopni wierzchołków
 
-# first analysis showed that k = 155 is highest
-#cutDegrees = cut_degree(largest_cc_graph, 156)
-#print_k_cores(cutDegrees)
-
-# DEBUG
-# print(dict(largest_cc_graph.degree()).items())
-# for node, degree in dict(largest_cc_graph.degree()).items():
-#     if degree > 2000:
-#         print('NODE: ' + node + ' | DEGREE: ' + str(degree))
-
-########################################################
-
-# Distribution of nodes degrees
 # degree_sequence = sorted([d for n, d in largest_cc_graph.degree()], reverse=True)
 # degreeCount = collections.Counter(degree_sequence)
 # deg, cnt = zip(*degreeCount.items())
 #
 # plt.bar(deg, cnt, width=0.10, color='b')
-#
-# plt.title("Degree Histogram")
-# plt.ylabel("Count")
-# plt.xlabel("Degree")
+# plt.title("Rozkład stopni wierzchołków")
+# plt.ylabel("Liczba wierzchołków")
+# plt.xlabel("Stopień")
 # axes = plt.gca()
 # axes.set_xlim([0,60])
-# axes.set_ylim([0,14000])
+# axes.set_ylim([0,40000])
 # plt.show()
 
-########################################################
-# wyznacz wykładnik rozkładu potęgowego metodą regresji dla dopełnienia dystrybuanty rozkładu stopni, dla przedziałów
-# rozlokowanych logarytmicznie
+# # # # wyznacz wykładnik rozkładu potęgowego metodą regresji dla dopełnienia dystrybuanty rozkładu stopni,
+# # # # dla przedziałów rozlokowanych logarytmicznie
 
-# wyznaczenie kubelkow
-res = np.logspace(1, 3, 7, dtype = int)
-# zapis stopni w. w liście
-degrees = [val for (node, val) in largest_cc_graph.degree()]
 
-# zliczenie w. o podanych st. w kubełkach
-degreeBasket = []
-for i in res:
-    ww = sum(1 for x in degrees if x > i)
-    degreeBasket.append(ww)
-
-# pobranie częstości st. w
-frequencyOfDegrees = nx.degree_histogram(largest_cc_graph)
-frequencyBaskets = []
-for i in res:
-    ww = frequencyOfDegrees[i]
-    frequencyBaskets.append(ww)
-
-# regresja
-# x = np.array(degreeBasket).reshape((-1,1))
+# TODO ffff
+# # wyznaczenie przedziałów logarytmicznych
+# logIntervals = np.logspace(1, 3, 7, dtype = int)
+# # zapis stopni wierzchołków w liście
+# degrees = [val for (node, val) in largest_cc_graph.degree()]
+#
+# # zliczenie wierzchołków w przedziałach
+# degreeBaskets = []
+# for i in logIntervals:
+#     intervalSum = sum(1 for x in degrees if x > i)
+#     degreeBaskets.append(intervalSum)
+#
+# # zliczenie częstotliwości stopni wierzchołków
+# frequencyOfDegrees = nx.degree_histogram(largest_cc_graph)
+# print(frequencyOfDegrees)
+#
+# frequencyBaskets = []
+# for i in logIntervals:
+#     frequencyInterval = frequencyOfDegrees[i]
+#     frequencyBaskets.append(frequencyInterval)
+#
+# print(frequencyBaskets)
+#
+# # regresja
+# x = np.array(degreeBaskets).reshape((-1, 1))
 # y = np.array(frequencyBaskets)
 #
-# model = LinearRegression().fit(x,y)
-# r_sq = model.score(x,y)
+# model = LinearRegression().fit(x, y)
+# r_sq = model.score(x, y)
 # pred = model.predict(x)
 #
-# print(str(degreeBasket))
-# print(str(frequencyBaskets))
-#
-# plt.plot(res, frequencyBaskets, 'b.')
-# plt.plot(res, pred, 'r-', label = 'linear regression')
-# plt.xlabel('Node degree')
-# plt.ylabel('Node frequency')
+# plt.plot(logIntervals, frequencyBaskets, 'b.')
+# plt.plot(logIntervals, pred, 'r-', label='linear regression')
+# plt.xlabel('Stopień wierzchołka')
+# plt.ylabel('Częstotliwość')
 # plt.legend()
 # plt.show()
 
@@ -168,13 +167,13 @@ for i in res:
 # plt.show()
 # print('alpha= ',fit.power_law.alpha,'  sigma= ',fit.power_law.sigma)
 
-print(frequencyOfDegrees)
-
-fit = powerlaw.Fit(np.array(frequencyOfDegrees) + 1, xmin= 1, discrete= True)
-fit.power_law.plot_pdf(color='b', linestyle='--',label='Fit ccdf')
-fit.plot_pdf( color= 'b')
-plt.show()
-print('alpha= ',fit.power_law.alpha,'  sigma= ',fit.power_law.sigma)
+# print(frequencyOfDegrees)
+#
+# fit = powerlaw.Fit(np.array(frequencyOfDegrees) + 1, xmin= 1, discrete= True)
+# fit.power_law.plot_pdf(color='b', linestyle='--',label='Fit ccdf')
+# fit.plot_pdf( color= 'b')
+# plt.show()
+# print('alpha= ',fit.power_law.alpha,'  sigma= ',fit.power_law.sigma)
 
 # fit = powerlaw.Fit(frequencyOfDegrees)
 # powerlaw.plot_pdf(frequencyOfDegrees)
@@ -186,7 +185,6 @@ print('alpha= ',fit.power_law.alpha,'  sigma= ',fit.power_law.sigma)
 # print(fit.xmin)
 # print(fit.alpha)
 # powerlaw.plot_pdf()
-
 
 
 # print('Largest connected component parameters')
