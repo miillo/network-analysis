a4a55ce47c7f7168078a76bf1425679bcc1171ef
Miłosz Pluta <milosz.pluta2@gmail.com>
2019-11-22 19:02:38 +0100

    Hill fix + refactor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import networkx as nx
 from random import choice
-import operator
-import itertools
 import collections
 import matplotlib.pyplot as plt
 import numpy as np
@@ -28,97 +26,97 @@ def no_of_nodes_for_k_cores(graph):
 # wczytanie grafu
 G = nx.read_adjlist('C:\\Users\\Milosz\\Desktop\\TASS_Project_I\\Ex2_data\\3.txt')
 
-# # # # # zbadaj jaki jest rząd i rozmiar całej sieci: pierwotnej oraz po usunięciu pętli i duplikatów krawędzi # # # #
+# # # # zbadaj jaki jest rząd i rozmiar całej sieci: pierwotnej oraz po usunięciu pętli i duplikatów krawędzi
 
-# print('Rząd i rozmiar sieci pierwotnej')
-# print('Liczba wierzchołków(rząd): ' + str(G.number_of_nodes()))
-# print('Liczba krawędzi(rozmiar): ' + str(G.number_of_edges()))
-#
-# print()
+print('Rząd i rozmiar sieci pierwotnej')
+print('Liczba wierzchołków(rząd): ' + str(G.number_of_nodes()))
+print('Liczba krawędzi(rozmiar): ' + str(G.number_of_edges()))
+
+print()
 
 # usunięcie pętli
 G.remove_edges_from(nx.selfloop_edges(G))
 # graf jest wczytany jako obiekt typu: 'networkx.classes.graph.Graph' - tzn. nie ma zduplikowanych krawędzi
 
-# print('Rząd i rozmiar sieci po usunięciu pętli / duplikató krawędzi')
-# print('Liczba wierzchołków(rząd): ' + str(G.number_of_nodes()))
-# print('Liczba krawędzi(rozmiar): ' + str(G.number_of_edges()))
-#
-# print()
+print('Rząd i rozmiar sieci po usunięciu pętli / duplikató krawędzi')
+print('Liczba wierzchołków(rząd): ' + str(G.number_of_nodes()))
+print('Liczba krawędzi(rozmiar): ' + str(G.number_of_edges()))
 
-# # # # # wyodrębnij największą składową spójną, zbadaj jej rząd i rozmiar # # # #
+print()
+
+# # # # wyodrębnij największą składową spójną, zbadaj jej rząd i rozmiar # # # #
 
 # sprawdzenie liczby komponentów
-# print('Liczba komponentów: ' + str(nx.number_connected_components(G)))
+print('Liczba komponentów: ' + str(nx.number_connected_components(G)))
 
 # generate largest connected component
 largest_cc = max(nx.connected_components(G), key=len)
 largest_cc_graph = G.subgraph(largest_cc)
 
-# print('Rząd i rozmiar największej składowej spójnej')
-# print('Liczba wierzchołków(rząd): ' + str(largest_cc_graph.number_of_nodes()))
-# print('Liczba krawędzi(rozmiar): ' + str(largest_cc_graph.number_of_edges()))
+print('Rząd i rozmiar największej składowej spójnej')
+print('Liczba wierzchołków(rząd): ' + str(largest_cc_graph.number_of_nodes()))
+print('Liczba krawędzi(rozmiar): ' + str(largest_cc_graph.number_of_edges()))
 
-# # # # # wyznacz aproksymacje średniej długości ścieżki,
-# # # # # operując na próbie losowej 100, 1000 i 10 tys. par wierzchołków
+# # # # wyznacz aproksymacje średniej długości ścieżki,
+# # # # operując na próbie losowej 100, 1000 i 10 tys. par wierzchołków
 
 # TODO dla trzech prob
 
-# sumOfShortestPaths = 0
-# noOfNodes = largest_cc_graph.number_of_nodes()
-# nodes = largest_cc_graph.nodes()
-#
-# for i in range(0, 100):
-#     node1 = choice(list(nodes))
-#     node2 = choice(list(nodes))
-#     shortest_path_length = nx.dijkstra_path_length(largest_cc_graph, node1, node2)
-#     sumOfShortestPaths += shortest_path_length
-#     print('Iteration: ' + str(i) + ' | Current sum: ' + str(sumOfShortestPaths))
-#
-# print(sumOfShortestPaths / (noOfNodes * (noOfNodes - 1)))
+sumOfShortestPaths = 0
+noOfNodes = largest_cc_graph.number_of_nodes()
+nodes = largest_cc_graph.nodes()
 
-# print()
+for i in range(0, 100):
+    node1 = choice(list(nodes))
+    node2 = choice(list(nodes))
+    shortest_path_length = nx.dijkstra_path_length(largest_cc_graph, node1, node2)
+    sumOfShortestPaths += shortest_path_length
+    print('Iteration: ' + str(i) + ' | Current sum: ' + str(sumOfShortestPaths))
 
-# # # # # wyznacz liczbę rdzeni o największym możliwym rzędzie, o drugim możliwie największym rzędzie
-# # # # # o trzecim możliwie największym rzędzie; jakie to są rzędy? # # # #
+print(sumOfShortestPaths / (noOfNodes * (noOfNodes - 1)))
+
+print()
+
+# # # # wyznacz liczbę rdzeni o największym możliwym rzędzie, o drugim możliwie największym rzędzie
+# # # # o trzecim możliwie największym rzędzie; jakie to są rzędy? # # # #
 
 # 'odmrożenie' grafu w celu umożliwienia wykonania na nim operacji
-# unfrezeed = nx.Graph(largest_cc_graph)
-#
-# print('Pierwsze sprawdzenie liczby rdzeni: ' + str(no_of_nodes_for_k_cores(unfrezeed)))  # max 155
-#
-# # wyznaczenie wierzchołków do usunięcia na podstawie ostatniego odczytu
-# to_del1 = [node for node, degree in dict(unfrezeed.degree()).items() if degree >= 155]
-# unfrezeed.remove_nodes_from(to_del1)
-#
-# print('Drugie sprawdzenie liczby rdzenii: ' + str(no_of_nodes_for_k_cores(unfrezeed))) # max 67
-#
-# # wyznaczenie wierzchołków do usunięcia na podstawie ostatniego odczytu
-# to_del2 = [node for node, degree in dict(unfrezeed.degree()).items() if degree >= 67]
-# unfrezeed.remove_nodes_from(to_del2)
-#
-# print('Trzecie sprawdzenie liczby rdzenii: ' + str(no_of_nodes_for_k_cores(unfrezeed))) # max 53
+unfrezeed = nx.Graph(largest_cc_graph)
+
+print('Pierwsze sprawdzenie liczby rdzeni: ' + str(no_of_nodes_for_k_cores(unfrezeed)))  # max 155
+
+# wyznaczenie wierzchołków do usunięcia na podstawie ostatniego odczytu
+to_del1 = [node for node, degree in dict(unfrezeed.degree()).items() if degree >= 155]
+unfrezeed.remove_nodes_from(to_del1)
+
+print('Drugie sprawdzenie liczby rdzenii: ' + str(no_of_nodes_for_k_cores(unfrezeed)))  # max 67
+
+# wyznaczenie wierzchołków do usunięcia na podstawie ostatniego odczytu
+to_del2 = [node for node, degree in dict(unfrezeed.degree()).items() if degree >= 67]
+unfrezeed.remove_nodes_from(to_del2)
+
+print('Trzecie sprawdzenie liczby rdzenii: ' + str(no_of_nodes_for_k_cores(unfrezeed)))  # max 53
 
 # # # # wykreśl rozkład stopni wierzchołków
 
-# degree_sequence = sorted([d for n, d in largest_cc_graph.degree()], reverse=True)
-# degreeCount = collections.Counter(degree_sequence)
-# deg, cnt = zip(*degreeCount.items())
-#
-# plt.bar(deg, cnt, width=0.10, color='b')
-# plt.title("Rozkład stopni wierzchołków")
-# plt.ylabel("Liczba wierzchołków")
-# plt.xlabel("Stopień")
-# axes = plt.gca()
-# axes.set_xlim([0,60])
-# axes.set_ylim([0,40000])
-# plt.show()
+degree_sequence = sorted([d for n, d in largest_cc_graph.degree()], reverse=True)
+degreeCount = collections.Counter(degree_sequence)
+deg, cnt = zip(*degreeCount.items())
+
+plt.bar(deg, cnt, width=0.10, color='b')
+plt.title("Rozkład stopni wierzchołków")
+plt.ylabel("Liczba wierzchołków")
+plt.xlabel("Stopień")
+axes = plt.gca()
+axes.set_xlim([0, 60])
+axes.set_ylim([0, 40000])
+plt.show()
 
 # # # # wyznacz wykładnik rozkładu potęgowego metodą regresji dla dopełnienia dystrybuanty rozkładu stopni,
 # # # # dla przedziałów rozlokowanych logarytmicznie
 
 # # wyznaczenie przedziałów logarytmicznych
-logIntervals = np.logspace(2.2, 3, 7, dtype = int)
+logIntervals = np.logspace(2.2, 3, 7, dtype=int)
 # # zapis stopni wierzchołków w liście
 degrees = [val for (node, val) in largest_cc_graph.degree()]
 
@@ -149,54 +147,10 @@ plt.ylabel('Częstotliwość')
 plt.legend()
 plt.show()
 
-# # # #
-# diagram hilla - pakiet lawpace
+# # # # wyznacz wykres Hilla
 
-# working sample # # #
-# d=[6, 4, 0, 0, 0, 0, 0, 1, 3, 1, 0, 3, 3, 0, 0, 0, 0, 1, 1, 0, 0, 0, 3,2,  3, 3, 2, 5, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 2, 1, 0, 1, 0, 0, 0, 0, 1,0, 1, 2, 0, 0, 0, 2, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1,3, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 2, 2, 3, 2, 1, 0, 0, 0, 1, 2]
-# fit = powerlaw.Fit(np.array(d)+1,xmin=1,discrete=True)
-# fit.power_law.plot_pdf( color= 'b',linestyle='--',label='fit ccdf')
-# fit.plot_pdf( color= 'b')
-# plt.show()
-# print('alpha= ',fit.power_law.alpha,'  sigma= ',fit.power_law.sigma)
-
-# print(frequencyOfDegrees)
-#
-# fit = powerlaw.Fit(np.array(frequencyOfDegrees) + 1, xmin= 1, discrete= True)
-# fit.power_law.plot_pdf(color='b', linestyle='--',label='Fit ccdf')
-# fit.plot_pdf( color= 'b')
-# plt.show()
-# print('alpha= ',fit.power_law.alpha,'  sigma= ',fit.power_law.sigma)
-
-# fit = powerlaw.Fit(frequencyOfDegrees)
-# powerlaw.plot_pdf(frequencyOfDegrees)
-# plt.show()
-# fig2 = fit.plot_pdf(color = 'b', linewidth = 2)
-# fit.power_law.plot_pdf(color = 'b')
-# print(fit.power_law.alpha)
-# fig2 = fit.plot_pdf(color = ‘b’, linewidth = 2)
-# print(fit.xmin)
-# print(fit.alpha)
-# powerlaw.plot_pdf()
-
-
-# print('Largest connected component parameters')
-# print('No. of nodes: ' + str(largest_cc_graph.number_of_nodes()))
-# print('No. of edges: ' + str(largest_cc_graph.number_of_edges()))
-
-
-print('\nEnd of processing')
-
-# ! ! ! Tutorial sample degree plot
-# all_deg = list(dict(nx.degree(largest_cc_graph)).values())
-# unique_deg = list(set(all_deg))
-# count_of_deg = []
-# for i in unique_deg:
-#     x = all_deg.count(i)
-#     count_of_deg.append(x)
-#
-# axes = plt.gca()
-# axes.set_xlim([0,60])
-#
-# plt.bar(unique_deg, count_of_deg)
-# plt.show()
+fit = powerlaw.Fit(np.array(degree_sequence) + 1, xmin=1, discrete=True)
+fit.power_law.plot_pdf(color='b', linestyle='--', label='PDF - wzorzec')
+fit.plot_pdf(color='g', label='PDF - dane[HILL]')
+plt.legend()
+plt.show()
